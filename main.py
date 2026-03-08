a4ce409a9258768993ee87f146c4b80afb01c12a
S-kipper <adityasekhartiwariup@gmail.com>
2026-03-08 16:25:34 +0530

    Learnt How to create a GET Route

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ app = Flask(__name__) #This line creates your web server application
 
 """We need to create a root which is basically an endpoint which is kinda a location on the API we can go to, to get some kind of data (We have different kinds of roots too)"""
 
+'''
 @app.route("/") #To make this accessible we use a decorator and the name of the application in this case its app in the bracket you add the path that we want to access in this case its "/" which is the default root  
 def home(): #Returning some data that the user will have access too when they reach this root
-    return "Home"
-
+    return "Home" 
+'''
+    
+'''Now we are about to use HTTP Methods to make more complex routes'''
+@app.route("/get-user/<user_id>") #<user-id> is known as a path parameter - (now a path parameter is a dynamic value that you can pass in the path an URL that we will be able to access inside of our root)
+def get_user(user_id):
+    user_data = {
+            "user_id" : user_id,
+            "name" : "Aditya",
+            "email" : "adi@exmple.com"
+    }
+    
+    extra = request.args.get("extra")    #args basically stores all the query parameters in a dictionary
+    if extra:
+        user_data["extra"] = extra
+        
+    return jsonify(user_data), 200 #200 here is called the Status code (it is the default status code for success)
+    
+    
 if __name__ == "__main__": #This is basically a python convention and checks if this file is running directly 
     app.run(debug = True) #http://127.0.0.1:5000  This the default server that runs the application
